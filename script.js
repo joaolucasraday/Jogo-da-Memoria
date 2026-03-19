0baea8b8dc200dec90d4f34c05df45b1ae4f6bd3
João Lucas Raday <lucasraday@gmail.com>
2026-03-19 08:55:06 -0300

    script de modos

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Scripts");
+function facil(){
+    localStorage.setItem("dificuldade", 1);
+}
+
+function medio(){
+    localStorage.setItem("dificuldade", 2);
+}
+
+function dificil(){
+    localStorage.setItem("dificuldade", 3);
+}
+
+let dificuldade = Number(localStorage.getItem("dificuldade"));
+console.log(dificuldade);
